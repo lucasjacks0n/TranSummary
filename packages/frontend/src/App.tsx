a62f21aea77867ae386a5aec52e08d242603cf77
Lucas Jackson <lucasjackson5815@gmail.com>
2023-11-22 20:20:37 +0000

    frontend improvements

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -9,7 +9,7 @@ import {
 } from "@mui/material";
 import "./App.css";
 import { parseVideo } from "./services/api";
-import { ISummary } from "./types/segment";
+import { IAudioSegment, IChapter, ISummary } from "./types/segment";
 import { formatTimestamp } from "./utils/helpers";
 import YouTube from "react-youtube";
 
@@ -22,7 +22,6 @@ const App = () => {
   const handleParseVideo = async () => {
     setLoading(true);
     await parseVideo(url).then((data) => {
-      console.log("update segments", data);
       setSummary(data);
       setLoading(false);
     });
@@ -34,6 +33,64 @@ const App = () => {
     player.seekTo(timestamp, true);
     player.playVideo();
   };
+
+  const FacesDisplay = (faces: string[]) => (
+    <Box>
+      <Grid container spacing={2}>
+        {faces.map((b64, index) => (
+          <Grid key={index} item xs={2}>
+            <Box
+              component="img"
+              sx={{ width: '100%', height: '100%' }}
+              alt="Face"
+              src={`data:image/jpg;base64, ${b64}`}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+
+  const Chapters = ({ chapters, onChapterSelect }: { chapters: IChapter[], onChapterSelect: (timestamp: number) => void }) => (
+    <Box>
+      {chapters.map((chapter, index) => (
+        <Box key={index} sx={{ py: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Button onClick={() => onChapterSelect(parseFloat(chapter.timestamp) / 1000)}>
+                {formatTimestamp(parseInt(chapter.timestamp) / 1000)}
+              </Button>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="body2" pt={1}>
+                {chapter.text}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      ))}
+    </Box>
+  );
+
+  const Transcript = ({ transcript, onSegmentSelect }: { transcript: IAudioSegment[], onSegmentSelect: (timestamp: number) => void }) => (
+    <Box>
+      {transcript.map((segment, index) => (
+        <Box key={index} sx={{ p: 1, borderBottom: '1px solid grey' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Typography variant="body2">{segment.speaker}</Typography>
+              <Button sx={{ p: 0 }} onClick={() => onSegmentSelect(segment.start_time / 1000)}>
+                {formatTimestamp(segment.start_time / 1000)} - {formatTimestamp(segment.end_time / 1000)}
+              </Button>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="body2">{segment.text}</Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      ))}
+    </Box>
+  );
 
   return (
     <Container>
@@ -62,98 +119,59 @@ const App = () => {
       {/* Main Content */}
       {summary && (
         <Grid container spacing={2} sx={{ maxHeight: "100px" }}>
-          <Grid item xs={8}>
-            <YouTube
-              style={{ width: "100%" }}
-              onPlaybackQualityChange={(e) => {
-                console.log("onPlaybackQualityChange", e.data);
-              }}
-              opts={{
-                playerVars: {
-                  autoplay: 0,
-                },
-                width: "100%",
-                height: "480",
-              }}
-              onError={(e) => {
-                console.log(e);
-              }}
-              ref={(ref) => (playerRef.current = ref)}
-              videoId={summary.videoId}
-            />
+          <Grid item xs={12} md={8}>
+            <Grid container spacing={2}>
+              <Grid xs={12} item>
+                <YouTube
+                  style={{ width: "100%" }}
+                  onPlaybackQualityChange={(e) => {
+                    console.log("onPlaybackQualityChange", e.data);
+                  }}
+                  opts={{
+                    playerVars: {
+                      autoplay: 0,
+                    },
+                    width: "100%",
+                    height: "480",
+                  }}
+                  onError={(e) => {
+                    console.log(e);
+                  }}
+                  ref={(ref) => (playerRef.current = ref)}
+                  videoId={summary.videoId}
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Typography variant="h4" component="h3">
+                  Faces
+                </Typography>
+                {FacesDisplay(summary?.faces || [])}
+              </Grid>
+              <Grid xs={12} item>
+                <Typography variant="h4" component="h3">
+                  Transcript
+                </Typography>
+                {Transcript({
+                  transcript: summary?.transcript || [],
+                  onSegmentSelect: (timestamp) => playerSkipTo(timestamp)
+                })}
+              </Grid>
+            </Grid>
           </Grid>
 
           {/* Right Column */}
-          <Grid item xs={4} sx={{ maxHeight: "480px" }}>
-
-            <Box sx={{ height: "100%", overflow: "scroll" }}>
+          <Grid item xs={12} md={4} sx={{ maxHeight: "480px" }}>
+            <Box>
               <Typography variant="h4" component="h3">
-                Faces
-              </Typography>
-              <Box>
-                <Grid spacing={2} container>
-                  {summary?.faces.map((b64) => {
-                    return (
-                      <Grid xs={4} item><Box
-                        component="img"
-                        sx={{
-                          height: '100%',
-                        }}
-                        alt="The house from the offer."
-                        src={`data:image/jpg;base64, ${b64}`} /></Grid>
-                    )
-                  })}
-                </Grid>
-              </Box>
-              <Typography pt={2} variant="h4" component="h3">
                 Chapters
               </Typography>
-              <Box>
-                {summary?.chapters.map((chapter) => {
-                  return (
-                    <Box sx={{ py: 1 }}>
-                      <Grid container spacing={2}>
-                        <Grid xs={4} item>
-                          <Button onClick={() => playerSkipTo(Math.floor(parseInt(chapter.timestamp) / 1000))}>
-                            {formatTimestamp(parseInt(chapter.timestamp) / 1000)}
-                          </Button>
-                        </Grid>
-                        <Grid xs={8} item>
-                          <Typography variant="body2" pt={1}>
-                            {chapter.text}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  );
-                })}
-              </Box>
-              <Typography variant="h4" component="h3">
-                Transcript
-              </Typography>
-              <Box>
-                {summary?.transcript.map((segment) => {
-                  return (
-                    <Box sx={{ p: 1, borderBottom: '1px solid grey' }}>
-                      <Grid container spacing={2}>
-                        <Grid xs={4} item>
-                          <Typography variant="body2">
-                            {segment.speaker}
-                          </Typography>
-                          <Button sx={{ p: 0 }} onClick={() => playerSkipTo(Math.floor(segment.start_time / 1000))}>
-                            {formatTimestamp(segment.start_time / 1000)} - {formatTimestamp(segment.end_time / 1000)}
-                          </Button>
-                        </Grid>
-                        <Grid xs={8} item>
-                          <Typography variant="body2">
-                            {segment.text}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  );
-                })}
-              </Box>
+              {Chapters({
+                chapters: summary?.chapters,
+                onChapterSelect: (timestamp) => {
+                  playerSkipTo(timestamp)
+                }
+              })}
+
             </Box>
           </Grid>
         </Grid>
